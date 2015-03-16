7e396d77b298c886ceee3e1a85b8a96acacf44d3
enixdark <cqshinn92@gmail.com>
2015-03-16 16:07:35 +0700

    update generics

--- tutorial_1/snippets/views.py
+++ tutorial_1/snippets/views.py
@@ -17,34 +17,18 @@ class JSONResponse(HttpResponse):
 		super(JSONResponse,self).__init__(content,**kwargs)
 
 
-class SnippetList(mixins.ListModelMixin,mixins.CreateModelMixin,
-					generics.GenericAPIView):
+class SnippetList(generics.ListCreateAPIView):
 
 	quereset = Snippet.objects.all()
 	serializer_class = SnippetSerializer
 
-	def get(self,request,*args,**kwargs):
 
-		return self.list(request,*args,**kwargs)
 
-	def post(self,request,*args,**kwargs):
-		return self.create(request,*args,**kwargs)
-
-class SnippetDetail(mixins.RetrieveModelMixin,
-					mixins.UpdateModelMixin,
-					mixins.DestroyModelMixin,
-					generics.GenericAPIView):
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
 
 	queryset = Snippet.objects.all()
 	serializer_class = SnippetSerializer
 
 
-	def get(self,request,*args,**kwargs):
-		return self.retrieve(request,*args,**kwargs)
 
-	def put(self,request,pk,format=None):
-		return self.update(request,*args,**kwargs)
-
-	def delete(self,request,pk):
-		return self.destroy(request,*args,**kwargs)
 
